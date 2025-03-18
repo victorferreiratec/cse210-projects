736290a20ea48351cc5a5396b751d2de5552bd9e
victorferreiratec <ferreira.tec.hugo@gmail.com>
2025-03-17 23:05:41 -0300

    modified:   week01/Exercise1/Program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+         Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+        
+        // Space between lines
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
